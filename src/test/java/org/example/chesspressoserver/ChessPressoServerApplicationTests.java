f7bd6909e706ed36ffd165ed5723e0d3cd18b64c
Michael <mikrametter@edu.aau.at>
2025-06-04 19:06:57 +0200

    Initial commit

---
+++ src/test/java/org/example/chesspressoserver/ChessPressoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chesspressoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessPressoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
